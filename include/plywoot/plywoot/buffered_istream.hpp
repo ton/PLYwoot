45d42abc3d24d0107d54ada0313b8f9167cfa5fd
Ton van den Heuvel <git@tonvandenheuvel.com>
2022-12-07 23:51:48 +0100

    Keep track of remaining bytes in `BufferedIStream`
    This improves performance of the binary parser by roughly 9% on a large
    binary input file.

--- include/plywoot/plywoot/buffered_istream.hpp
+++ include/plywoot/plywoot/buffered_istream.hpp
@@ -46,8 +46,9 @@ public:
   {
     // Note; buffer a bit more than strictly necessary, so that we can move the
     // read head unconditionally after reading the object of type T.
-    buffer(sizeof(T) * 2);
+    if (remaining_ < sizeof(T)) buffer(sizeof(T));
     const char *t = c_;
+    remaining_ -= sizeof(T);
     c_ += sizeof(T);
     return *reinterpret_cast<const T *>(t);
   }
@@ -57,21 +58,21 @@ public:
   inline void readCharacter()
   {
     ++c_;
-    if (c_ >= eob_) { buffer(); }
+    --remaining_;
+    if (remaining_ == 0) { buffer(); }
   }
 
   /// Skips the given number of bytes in the input stream.
   void skip(std::size_t n)
   {
-    const std::size_t remaining = eob_ - c_;
-    if (remaining > n)
+    if (remaining_ > n)
     {
       c_ += n;
+      remaining_ -= n;
     }
     else
     {
-      n -= remaining;
-      is_.seekg(n, std::ios_base::cur);
+      is_.seekg(n - remaining_, std::ios_base::cur);
       buffer();
     }
   }
@@ -83,9 +84,10 @@ public:
   {
     while (*c_ != EOF && n > 0)
     {
-      c_ = static_cast<const char *>(std::memchr(c_, '\n', eob_ - c_));
+      c_ = static_cast<const char *>(std::memchr(c_, '\n', remaining_));
       if (c_)
       {
+        remaining_ = eob_ - c_;
         readCharacter();
         --n;
       }
@@ -128,16 +130,19 @@ public:
   void buffer(size_t minimum)
   {
     assert(minimum < BufferSize / 2);
-    const size_t remaining = eob_ - c_;
-    if (remaining < minimum)
+    if (remaining_ < minimum)
     {
-      std::memcpy(buffer_, c_, remaining);
-      if (!is_.read(buffer_ + remaining, BufferSize - remaining))
+      std::memcpy(buffer_, c_, remaining_);
+      if (!is_.read(buffer_ + remaining_, BufferSize - remaining_))
       {
         // In case the buffer is only partially filled, fill the remainder with
         // EOF characters.
-        auto bufferedBytes = is_.gcount() + remaining;
-        std::fill_n(buffer_ + bufferedBytes, BufferSize - bufferedBytes, EOF);
+        remaining_ = is_.gcount() + remaining_;
+        std::fill_n(buffer_ + remaining_, BufferSize - remaining_, EOF);
+      }
+      else
+      {
+        remaining_ = BufferSize;
       }
 
       c_ = buffer_;
@@ -152,8 +157,12 @@ private:
     {
       // In case the buffer is only partially filled, fill the remainder with
       // EOF characters.
-      auto bufferedBytes = is_.gcount();
-      std::fill_n(buffer_ + bufferedBytes, BufferSize - bufferedBytes, EOF);
+      remaining_ = is_.gcount();
+      std::fill_n(buffer_ + remaining_, BufferSize - remaining_, EOF);
+    }
+    else
+    {
+      remaining_ = BufferSize;
     }
 
     c_ = buffer_;
@@ -169,6 +178,8 @@ private:
   const char *c_{buffer_ + BufferSize};
   /// End-of-buffer pointer.
   const char *eob_{c_};
+  /// Number of bytes remaining in the buffer.
+  std::size_t remaining_{0};
 
   /// Reference to the wrapper standard input stream.
   std::istream &is_;
